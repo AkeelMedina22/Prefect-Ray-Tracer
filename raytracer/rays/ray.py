c0aa7190683a959dd188e22ec9ffe3160043eda1
AkeelMedina22 <akeelmedina22@gmail.com>
2024-02-11 09:48:03 +0500

    Chapter 5.1

--- raytracer/rays/ray.py
+++ raytracer/rays/ray.py
@@ -7,3 +7,9 @@ class Ray:
     def __init__(self, origin: Point, direction: Vector):
         self.origin: Point = origin
         self.direction: Vector = direction
+
+    def position(self, t: float) -> Point:
+        return self.origin + self.direction * t
+
+    def intersect(self, shape):
+        return shape.intersect(self)
